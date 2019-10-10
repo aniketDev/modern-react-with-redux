e64f203d29d33bd6614ba5fae31fd66e498805fb
Aniket Mandal <aniketmandal9@gmail.com>
2019-10-11 02:33:34 +0530

    async await  when fetching response

--- src/ImageSearchApp.js
+++ src/ImageSearchApp.js
@@ -3,17 +3,16 @@ import axios from "axios";
 import SearchBar from "./Components/ImageSearch/SearchBar";
 
 class ImageSearchApp extends React.Component {
-  onSearchSubmit = term => {
-    axios
-      .get("https://api.unsplash.com/search/photos", {
-        params: { query: term },
-        headers: {
-          Authorization:
-            "Client-ID a95297c93c3024004daf90267504625eb6b3074b3a33ee5e470275fd44ac06c6"
-        }
-      })
-      .then(response => console.log(response.data.results));
-  };
+  async onSearchSubmit(term) {
+    const response = await axios.get("https://api.unsplash.com/search/photos", {
+      params: { query: term },
+      headers: {
+        Authorization:
+          "Client-ID a95297c93c3024004daf90267504625eb6b3074b3a33ee5e470275fd44ac06c6"
+      }
+    });
+    console.log(response.data.results);
+  }
 
   render() {
     return (
