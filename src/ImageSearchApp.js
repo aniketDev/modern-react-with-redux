0de9646bd576d64a3cc054c2f641cdac3f2aa0c0
Aniket Mandal <aniketmandal9@gmail.com>
2019-10-11 02:43:35 +0530

    SHow response result using state

--- src/ImageSearchApp.js
+++ src/ImageSearchApp.js
@@ -3,6 +3,7 @@ import axios from "axios";
 import SearchBar from "./Components/ImageSearch/SearchBar";
 
 class ImageSearchApp extends React.Component {
+  state = { images: [] };
   async onSearchSubmit(term) {
     const response = await axios.get("https://api.unsplash.com/search/photos", {
       params: { query: term },
@@ -11,13 +12,14 @@ class ImageSearchApp extends React.Component {
           "Client-ID a95297c93c3024004daf90267504625eb6b3074b3a33ee5e470275fd44ac06c6"
       }
     });
-    console.log(response.data.results);
+    this.setState({ images: response.data.results });
   }
 
   render() {
     return (
       <div className="ui container" style={{ marginTop: "10px" }}>
-        <SearchBar onSubmit={this.onSearchSubmit} />
+        <SearchBar onSubmit={this.onSearchSubmit.bind(this)} />
+        Found {this.state.images.length} images
       </div>
     );
   }
