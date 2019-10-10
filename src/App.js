04f1439dc0718b68c5ebd6a536ab71bc7f354b05
Aniket Mandal <aniketmandal9@gmail.com>
2019-10-10 19:58:07 +0530

    Refactored entire project

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from "react";
+import BlogApp from "./BlogApp";
+import SeasonApp from "./SeasonApp";
+
+const App = () => {
+  return (
+    <div>
+      <SeasonApp />
+    </div>
+  );
+};
+
+export default App;
