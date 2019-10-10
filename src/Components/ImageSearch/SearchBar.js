d9a20ef6c9eb70b4472d7b5b863c33a4cd3ab515
Aniket Mandal <aniketmandal9@gmail.com>
2019-10-11 00:28:24 +0530

    'this' problem solution 2

--- src/Components/ImageSearch/SearchBar.js
+++ src/Components/ImageSearch/SearchBar.js
@@ -3,15 +3,15 @@ import React from "react";
 class SearchBar extends React.Component {
   state = { term: "" };
 
-  onFormSubmit(event) {
+  onFormSubmit = event => {
     event.preventDefault();
     console.log(this.state.term);
-  }
+  };
 
   render() {
     return (
       <div className="ui segment">
-        <form className="ui form" onSubmit={this.onFormSubmit.bind(this)}>
+        <form className="ui form" onSubmit={this.onFormSubmit}>
           <div className="field">
             <label>Image Search</label>
             <input
