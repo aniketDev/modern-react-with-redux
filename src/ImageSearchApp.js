d07b1e9a243b8956c129c0bbc43ddc752132f1de
Aniket Mandal <aniketmandal9@gmail.com>
2019-10-11 00:49:28 +0530

    Communication child to parent: Invoking callbacks in children

--- src/ImageSearchApp.js
+++ src/ImageSearchApp.js
@@ -2,10 +2,14 @@ import React from "react";
 import SearchBar from "./Components/ImageSearch/SearchBar";
 
 class ImageSearchApp extends React.Component {
+  onSearchSubmit = term => {
+    console.log("submit called " + term);
+  };
+
   render() {
     return (
       <div className="ui container" style={{ marginTop: "10px" }}>
-        <SearchBar />
+        <SearchBar onSubmit={this.onSearchSubmit} />
       </div>
     );
   }
