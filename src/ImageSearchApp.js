51ac1d9a33fb8d6ea1a768aa0e7f671c8fe0438c
Aniket Mandal <aniketmandal9@gmail.com>
2019-10-11 02:21:16 +0530

    Install axios and set axios get method with unsplash api

--- src/ImageSearchApp.js
+++ src/ImageSearchApp.js
@@ -1,9 +1,17 @@
 import React from "react";
+import axios from "axios";
 import SearchBar from "./Components/ImageSearch/SearchBar";
 
 class ImageSearchApp extends React.Component {
+  state;
   onSearchSubmit = term => {
-    console.log("submit called " + term);
+    axios.get("https://api.unsplash.com/search/photos", {
+      params: { query: term },
+      headers: {
+        Authorization:
+          "Client-ID a95297c93c3024004daf90267504625eb6b3074b3a33ee5e470275fd44ac06c6"
+      }
+    });
   };
 
   render() {
