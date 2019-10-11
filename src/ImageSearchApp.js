5d90ec8b0c024899da9429aaa6bb6d277e903846
Aniket Mandal <aniketmandal9@gmail.com>
2019-10-11 11:25:46 +0530

    async await arrow function

--- src/ImageSearchApp.js
+++ src/ImageSearchApp.js
@@ -4,7 +4,7 @@ import SearchBar from "./Components/ImageSearch/SearchBar";
 
 class ImageSearchApp extends React.Component {
   state = { images: [] };
-  async onSearchSubmit(term) {
+  onSearchSubmit = async term => {
     const response = await axios.get("https://api.unsplash.com/search/photos", {
       params: { query: term },
       headers: {
@@ -13,12 +13,12 @@ class ImageSearchApp extends React.Component {
       }
     });
     this.setState({ images: response.data.results });
-  }
+  };
 
   render() {
     return (
       <div className="ui container" style={{ marginTop: "10px" }}>
-        <SearchBar onSubmit={this.onSearchSubmit.bind(this)} />
+        <SearchBar onSubmit={this.onSearchSubmit} />
         Found {this.state.images.length} images
       </div>
     );
