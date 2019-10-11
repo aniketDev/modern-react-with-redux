421c7ce5bbd1875223cb8d909e3635d016a92693
Aniket Mandal <aniketmandal9@gmail.com>
2019-10-11 18:55:36 +0530

    Image list css and destructuring in maps

--- src/Components/ImageSearch/ImageList.js
+++ src/Components/ImageSearch/ImageList.js
@@ -1,17 +1,12 @@
 import React from "react";
+import "./ImageList.css";
 
 const ImageList = props => {
-  const images = props.images.map(image => {
-    return (
-      <img
-        src={image.urls.regular}
-        alt={image.alt_description}
-        key={image.id}
-      />
-    );
+  const images = props.images.map(({ id, alt_description, urls }) => {
+    return <img src={urls.regular} alt={alt_description} key={id} />;
   });
 
-  return <div>{images}</div>;
+  return <div className="image-list">{images}</div>;
 };
 
 export default ImageList;
