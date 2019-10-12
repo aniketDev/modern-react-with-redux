4b56e86a82d20ef1ce97e989b84b2686a65e1148
Aniket Mandal <aniketmandal9@gmail.com>
2019-10-12 18:33:37 +0530

    ref in react

--- src/Components/ImageSearch/ImageCard.js
+++ src/Components/ImageSearch/ImageCard.js
@@ -1,10 +1,16 @@
 import React from "react";
 
 class ImageCard extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.imageRef = React.createRef();
+  }
+
   render() {
     const { alt_description, urls } = this.props.image;
 
-    return <img alt={alt_description} src={urls} />;
+    return <img ref={this.imageRef} alt={alt_description} src={urls.regular} />;
   }
 }
 
