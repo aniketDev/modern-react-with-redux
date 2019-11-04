c17486258897221b220075f53396bd00a8e9a12b
Aniket Mandal <aniketmandal9@gmail.com>
2019-11-04 22:57:12 +0530

    Shorthand syntax with redux thunk

--- src/Actions/BlogAppActions.js
+++ src/Actions/BlogAppActions.js
@@ -1,8 +1,6 @@
 import jsonPlacehoder from '../Api/jsonPlacehoder';
 
-export const fetchPosts = () => {
-  return async function(dispatch, getState) {
-    const response = await jsonPlacehoder.get('/posts');
-    dispatch({ type: 'FETCH_POSTS', payload: response });
-  };
+export const fetchPosts = () => async dispatch => {
+  const response = await jsonPlacehoder.get('/posts');
+  dispatch({ type: 'FETCH_POSTS', payload: response });
 };
