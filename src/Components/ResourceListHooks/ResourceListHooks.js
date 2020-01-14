c08f02c437dcedbc7d55253962aacc72b0a5151a
Aniket Mandal <aniketmandal9@gmail.com>
2020-01-14 16:55:18 +0530

    Reuse resourcelist and render user names

--- src/Components/ResourceListHooks/ResourceListHooks.js
+++ src/Components/ResourceListHooks/ResourceListHooks.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import useResources from './useResources';
+import UserList from './UserList';
 
 const ResourceListHooks = ({ resource }) => {
   const resources = useResources(resource);
 
   return (
     <div>
+      <UserList />
       <ul>
         {resources.map(data => (
           <li key={data.id}>{data.title}</li>
