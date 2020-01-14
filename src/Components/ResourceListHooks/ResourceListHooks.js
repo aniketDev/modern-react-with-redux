33cb034b8d8f5a399c8a124bc0aff4404d56772d
Aniket Mandal <aniketmandal9@gmail.com>
2020-01-14 15:15:10 +0530

    Rendered list of data

--- src/Components/ResourceListHooks/ResourceListHooks.js
+++ src/Components/ResourceListHooks/ResourceListHooks.js
@@ -1,10 +1,23 @@
 import React from 'react';
+import axios from 'axios';
 
 class ResourceListHooks extends React.Component {
+  state = { resources: [] };
+
+  async componentDidMount() {
+    const response = await axios.get(`https://jsonplaceholder.typicode.com/${this.props.resource}`);
+
+    this.setState({ resources: response.data });
+  }
+
   render() {
     return (
       <div>
-        {this.props.resource}
+        <ul>
+          {this.state.resources.map(data => (
+            <li>{data.title}</li>
+          ))}
+        </ul>
       </div>
     )
   }
